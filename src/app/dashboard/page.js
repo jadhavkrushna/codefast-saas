0ffeb7669b3e8f04521784e1dfaa175b1f96af6c
jadhavkrushna <jadhavkrishna25751@gmail.com>
2026-07-16 14:10:35 +0530

    feat: create initial dashboard page structure with product information and call to action

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Dashboard</h1>
+            <p>codefast is a saas platform that helps you collect customer feedback to build better products. get started for free.</p>
+            <button>Get started</button>
+        </main>
+    );
+}
