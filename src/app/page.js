89a3529b8561548daf456a29eff68d7fb9451990
jadhavkrushna <jadhavkrishna25751@gmail.com>
2026-07-16 14:10:14 +0530

    feat: implement landing page, login button component, and dashboard route

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
       <h1>Collect customer feedback to build better products</h1>
       <p>Codefast is a SaaS platform that helps you collect customer feedback to build better products. Get started for free.</p>
-
+      <ButtonLogin />
     </main>
   );
 }
